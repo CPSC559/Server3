4a59e86bc4554f0f60465224f644ec2600e965d4
Max Botham <maxsspamemails@gmail.com>
2024-02-18 14:00:13 -0700

    Socket initial

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+
+//Remove cors
+app.use(cors());
+
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:3000", // Allow only the React client to connect
+        methods: ["GET", "POST"] // Allow only these methods in CORS requests
+      }
+});
 
 io.on('connection', (socket) => {
-  console.log('New client connected');
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+    console.log('Client connected');
+  
+    socket.on('disconnect', (reason) => {
+      console.log(`Client disconnected, reason: ${reason}`);
+    });
+  
+    socket.on('error', (error) => {
+      console.error(`Connection error: ${error}`);
+    });
   });
-});
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`Listening on port ${port}`));
