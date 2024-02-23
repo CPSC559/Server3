92e91230b7b448765f24481946586b4519b96e50
Max Botham <maxsspamemails@gmail.com>
2024-02-23 16:09:03 -0700

    Make updates to socket / replace unecessary endpoitns

--- index.js
+++ index.js
@@ -25,6 +25,46 @@ const io = socketIo(server, {
 io.on("connection", (socket) => {
   console.log("Client connected");
 
+  socket.on("send_message", async (data) => {
+
+    const { message, user, chatroom } = data;
+    console.log(`Message received: ${message} from ${user} for chatroom ${chatroom}`);
+    try {
+      const message = await Message.create({
+        MessageContent: message,
+        User: user,
+        ChatroomID: chatroom,
+      });
+      io.to(chatroom).emit("new_message", message); // Broadcast the new message to the client(s) connected to the chatroom
+      res.status(200).json(message);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }    
+  });
+  
+  socket.on("join_chatroom", async ({ chatroomID, password }) => {
+    console.log(`Socket ${socket.id} requesting to join chatroom ${chatroomID} with password`);
+
+    // Verify chatroom password
+    try {
+      const chatroom = await Chatroom.findOne({ _id: chatroomID });
+      if (chatroom && chatroom.Password === password) {
+        // Password is correct, join the chatroom
+        socket.join(chatroomID);
+        console.log(`Socket ${socket.id} joined chatroom ${chatroomID}`);
+        // Optionally, confirm to the client they have joined the room
+        socket.emit("joined_chatroom", { success: true, chatroomID });
+      } else {
+        // Password is incorrect or chatroom does not exist
+        console.log(`Socket ${socket.id} failed to join chatroom ${chatroomID}`);
+        socket.emit("joined_chatroom", { success: false, error: "Invalid chatroom ID or password" });
+      }
+    } catch (error) {
+      console.error(`Error finding chatroom: ${error}`);
+      socket.emit("joined_chatroom", { success: false, error: "Server error while joining chatroom" });
+    }
+  });
+
   socket.on("disconnect", (reason) => {
     console.log(`Client disconnected, reason: ${reason}`);
   });
@@ -33,6 +73,7 @@ io.on("connection", (socket) => {
     console.error(`Connection error: ${error}`);
   });
 });
+
 const uri =
   "mongodb+srv://AppUser:qvRGUENrpuplSpeT@cpsc559project.uhkbb5v.mongodb.net/CPSC559Project?retryWrites=true&w=majority";
 
@@ -73,6 +114,8 @@ app.get("/chatroom/:id/messages", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
+//This is not necessary AFAI Understand
 //Example for how to call the following endpoint http://localhost:4000/message?message=Content&user=Test User&chatroom=65d691acb3615c49d737f639
 //Endpoint can be used to send new message data to the db
 app.post("/message", async (req, res) => {
